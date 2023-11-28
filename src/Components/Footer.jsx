d588e5a998ff6fb8bfd629886a64f0b7ffd56686
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-11-28 00:37:54 -0500

    Started footer setup

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='bigFt'>
+            <div className='shopping'>
+                <h3>Shopping</h3>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+            <div className='mission'>
+                <h3>Mission in Action</h3>
+                    <ul>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+            </div>
+            <div className='about'>
+                <h3>About</h3>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul></div>
+            <div className='help'>
+                <h3>Need Help?</h3>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+                <h3>Connect With Us</h3>
+                <div>
+                    {/* social media imgs */}
+                </div>
+                <button>Sign up for email</button>
+            </div>
+        </div>
+        <div className='lilFt'>
+            <div className='list'>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
